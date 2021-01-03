a4fcc862b0c644da4e9afb0f25f5ea42613ae716
Melvin Thomas <melvinchooranolil@gmail.com>
2021-01-04 02:05:44 +0530

    enthokkeyo

--- badge_generator/settings.py
+++ badge_generator/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -93,6 +96,9 @@ DATABASES = {
 }
 
 
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -140,3 +146,5 @@ os.path.join(BASE_DIR, 'static'),
 
 # root = os.path.dirname(os.path.abspath(__file__))
 # print(root)
+
+django_heroku.settings(locals())
