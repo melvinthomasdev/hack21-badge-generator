2c68d5d4501bb9eadf79c73923dec853c63754a4
Melvin Thomas <melvinchooranolil@gmail.com>
2021-01-04 11:59:12 +0530

    static urls added

--- badge_generator/urls.py
+++ badge_generator/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import static
+from django.conf import settings
 
 from app.views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
