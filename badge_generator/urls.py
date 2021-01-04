a5bf672e38db3747e032b181501d9e71b35af338
Melvin Thomas <melvinchooranolil@gmail.com>
2021-01-04 12:54:38 +0530

    static updated

--- badge_generator/urls.py
+++ badge_generator/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import static
+from django.conf.urls.static import static
 from badge_generator.settings import  STATIC_ROOT, STATIC_URL
 
 from app.views import index_view
