1f8962b2379ec75863241e5629a79d08540449e6
Melvin Thomas <melvinchooranolil@gmail.com>
2021-08-06 12:58:10 +0530

    template update

--- app/views.py
+++ app/views.py
@@ -90,7 +90,7 @@ def index_view(request):
                 print(thumbimg.size)
                 image_copy = template.copy()
                 image_copy.convert("RGBA")
-                position = (315, 319)
+                position = (260, 319)
                 image_copy.paste(thumbimg, position, thumbimg)
                 draw = ImageDraw.Draw(image_copy)
                 # name = "john".title()
