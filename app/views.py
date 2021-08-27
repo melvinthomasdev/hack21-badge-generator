a19a2c8088dcac294863625fb8e7fd8580520328
Melvin Thomas <melvinchooranolil@gmail.com>
2021-08-27 19:20:24 +0530

    xtreme ambassadors badge generator

--- app/views.py
+++ app/views.py
@@ -11,8 +11,8 @@ from .forms import ImageUploadForm
 
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # print(root)
-template_url = root + staticfiles_storage.url("img\spar.jpg")
-fontname_url = root + staticfiles_storage.url('fonts\Lobster_1_4.otf')
+template_url = root + staticfiles_storage.url(r'img\xtreme.png')
+fontname_url = root + staticfiles_storage.url(r'fonts\Roboto-Regular.ttf')
 # circle_image_url = root
 template = Image.open(template_url)
 
@@ -60,33 +60,34 @@ def index_view(request):
         if form.is_valid():
             if request.FILES:
                 src_img = request.FILES.get("image")
-                name = form.cleaned_data.get('name').title()
+                # name = " ".join(form.cleaned_data.get('name').title().split())
+                # college_name = " ".join(form.cleaned_data.get('college_name').title().split())
                 # context['img'] = src_img
                 #width, height
                 thumbimg = Image.open(src_img)
                 img_width, img_height = thumbimg.size
                 if thumbimg.width > thumbimg.height:
-                    reqht = 262
-                    reqwd = int((thumbimg.width/thumbimg.height)*262)
+                    reqht = 688
+                    reqwd = int((thumbimg.width/thumbimg.height)*688)
                 if thumbimg.width < thumbimg.height:
-                    reqwd = 262
-                    reqht = int((thumbimg.height/thumbimg.width)*262)
+                    reqwd = 688
+                    reqht = int((thumbimg.height/thumbimg.width)*688)
                 if thumbimg.width == thumbimg.height:
-                    reqwd = reqht = 262
+                    reqwd = reqht = 688
 
                 thumbimg = thumbimg.resize((reqwd, reqht))
                 width, height = thumbimg.size
-                left = (width - 262)/2
-                top = (height - 262)/2
-                right = (width + 262)/2
-                bottom = (height + 262)/2
+                left = (width - 688)/2
+                top = (height - 688)/2
+                right = (width + 688)/2
+                bottom = (height + 688)/2
                 thumbimg = thumbimg.crop((left, top, right, bottom))
                 # im_square = crop_max_square(thumbimg).resize((reqwd, reqwd), Image.LANCZOS)
                 # thumbimg = mask_circle_transparent(im_square, 0)
                 # thumbimg.save('circle.png')
                 # thumbimg = Image.open('circle.png')
                 # thumbimg.save(response, 'png')
-                thumbimg = thumbimg.resize((262, 262))
+                thumbimg = thumbimg.resize((688, 688))
                 bigsize = (thumbimg.size[0] * 3, thumbimg.size[1] * 3)
                 mask = Image.new('L', bigsize, 0)
                 draw = ImageDraw.Draw(mask) 
@@ -99,32 +100,61 @@ def index_view(request):
                 print(thumbimg.size)
                 image_copy = template.copy()
                 image_copy.convert("RGBA")
-                position = (315, 260)
+                position = (196, 616)
                 image_copy.paste(thumbimg, position, thumbimg)
                 draw = ImageDraw.Draw(image_copy)
                 # name = "john".title()
-                fontsize = get_fontsize(thumbimg, name)
-                fontsize = 80 if fontsize>80 else fontsize
-                print('font size',fontsize)
-                # (x, y) = (79,970)
-                if fontsize<40:
-                    fontsize = get_fontsize(thumbimg, name, 2.3)
-                    print("After adjusting:", fontsize)
-                    # (x, y) = (79, 970)
-                font = ImageFont.truetype(fontname_url, size=fontsize)
-                color = 'rgb(000,000,000)' # white color
-                strip_width, strip_height = 875, 100
-                label = Image.new("RGBA", (strip_width,strip_height), (0,0,0,0))
-                draw = ImageDraw.Draw(label)
-                label.putalpha(1)
-                draw = ImageDraw.Draw(label)
-                text_width, text_height = draw.textsize(name, font)
-                position = ((strip_width-text_width)/2,(strip_height-text_height)/2)
-                draw.text(position, name, color, font=font)
-                name_position = (0,622)
-                image_copy.paste(label, name_position, label)
+
+
+
+                # name_fontsize = get_fontsize(thumbimg, name)
+                # name_fontsize = 80 if name_fontsize>80 else fontsize
+                # print('font size',name_fontsize)
+                # # (x, y) = (79,970)
+                # if name_fontsize<50:
+                #     name_fontsize = get_fontsize(thumbimg, name, 2.7)
+                #     print("After adjusting:", name_fontsize)
+                #     # (x, y) = (79, 970)
+                # font = ImageFont.truetype(fontname_url, size=name_fontsize)
+                # color = 'rgb(000,000,000)' # white color
+                # strip_width, strip_height = 822, 74
+                # name_label = Image.new("RGBA", (strip_width,strip_height), (0,0,0,0))
+                # draw = ImageDraw.Draw(name_label)
+                # name_label.putalpha(1)
+                # draw = ImageDraw.Draw(name_label)
+                # text_width, text_height = draw.textsize(name, font)
+                # position = ((strip_width-text_width)/2,(strip_height-text_height)/2)
+                # draw.text(position, name, color, font=font)
+                # name_position = (152,1372)
+                # image_copy.paste(name_label, name_position, name_label)
+
+
+
+                # college_name_fontsize = get_fontsize(thumbimg, college_name)
+                # college_name_fontsize = 45 if college_name_fontsize>45 else college_name_fontsize
+                # print('font size',college_name_fontsize)
+                # # (x, y) = (79,970)
+                # if college_name_fontsize<50:
+                #     college_name_fontsize = get_fontsize(thumbimg, name, 2.3)
+                #     print("After adjusting:", college_name_fontsize)
+                #     # (x, y) = (79, 970)
+                # font = ImageFont.truetype(fontname_url, size=college_name_fontsize)
+                # color = 'rgb(000,000,000)' # white color
+                # strip_width, strip_height = 822, 74
+                # college_name_label = Image.new("RGBA", (strip_width,strip_height), (0,0,0,0))
+                # draw = ImageDraw.Draw(college_name_label)
+                # college_name_label.putalpha(1)
+                # draw = ImageDraw.Draw(college_name_label)
+                # text_width, text_height = draw.textsize(college_name, font)
+                # position = ((strip_width-text_width)/2,(strip_height-text_height)/2)
+                # draw.text(position, college_name, color, font=font)
+                # college_name_position = (152,1446)
+                # image_copy.paste(college_name_label, college_name_position, college_name_label)
+
+
                 # image_copy.save('pasted_image.png')
-                response['Content-Disposition'] = 'attachment; filename=%s.png' % name
+                # response['Content-Disposition'] = 'attachment; filename=%s.png' % name
+                response['Content-Disposition'] = 'attachment; filename=%s.png' % "IEEEXtreme Badge"
                 image_copy.save(response, 'png')
                 return response
         else:
@@ -132,5 +162,3 @@ def index_view(request):
     else:
         context['form'] = ImageUploadForm()
     return render(request, 'index.html', context)
-
-
